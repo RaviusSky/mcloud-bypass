ff6355befb68b80a62b8d6d48ebb2c6e215697c6
Cole Bianchi <RaviusSky@users.noreply.github.com>
2020-02-06 02:03:40 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+var { exec } = require('child_process')
+var http = require('http')
+
+var port = process.env.PORT
+
+http.createServer(function(req, res)
+{
+	if (req.url == "/favicon.ico")
+	{
+		res.end()
+		return
+	}
+
+	console.log(req.url.substr(1))
+
+	exec('curl '+req.url.substr(1), (err, stdout, stderr) =>
+	{
+		if (err)
+		{
+			console.error(err)
+		}
+
+		res.write(stdout)
+		res.end();
+	});
+}).listen(port)
